d322102e6da479422e6dbcf81de32afbe523726c
megayucke <47730564+megayucke@users.noreply.github.com>
2019-07-13 17:31:55 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, abort, request, redirect
+from dyslexia import *
 
 app = Flask(__name__)
 
@@ -17,3 +18,18 @@ def test():
     currLine = "and Ham sam!"
     postLine = "this is postline!"
     return render_template('test.html', preLine = preLine, currLine = currLine, postLine = postLine)
+
+
+@app.route('/lineByLine', methods=["GET","POST"])
+def lineByLine():
+
+    if request.method=="POST":  # If post request i.e. client clicks
+        clientSaid = Record(data)
+        return render_template('lineByLine.html', textSaid = clientSaid)
+
+    # OTHERWISE
+    fileName = getFile()
+    with open(fileName, 'r') as file:
+        data = file.read()
+        dataLower = data.lower()
+    return render_template('lineByLine.html', textSaid="", textToRead = data)
